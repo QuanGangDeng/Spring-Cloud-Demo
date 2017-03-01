fbd7f6a0026f4cda8d6ecb2246f460ef54cef885
James Deng <quangang.deng@gmail.com>
2017-03-01 15:41:56 +0800

    eureka client

---
+++ eureka.client/src/main/java/springCloud/eureka/client/App.java
@@ -0,0 +1,25 @@
+package springCloud.eureka.client;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("eurake.client");
+		return "Hello world 我是 eurake.client ";
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(App.class).web(true).run(args);
+	}
+}
