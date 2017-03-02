db6514c79471493945b587a9d2b312fa82982cd6
James Deng <quangang.deng@gmail.com>
2017-03-02 15:31:58 +0800

    spring cloud zuul 服务网关gate way使用

---
+++ eureka.zuul/src/main/java/springCloud/eureka/zuul/App.java
@@ -0,0 +1,19 @@
+package springCloud.eureka.zuul;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class App {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(App.class).web(true).run(args);
+	}
+	
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+}
