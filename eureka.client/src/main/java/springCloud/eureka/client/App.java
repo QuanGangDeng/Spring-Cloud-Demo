f3871c31d8d45cad86ddf80e17dbefc00637d893
James Deng <quangang.deng@gmail.com>
2017-03-03 10:21:03 +0800

    eureka client 指定eureka server cluster

--- eureka.client/src/main/java/springCloud/eureka/client/App.java
+++ eureka.client/src/main/java/springCloud/eureka/client/App.java
@@ -1,6 +1,5 @@
 package springCloud.eureka.client;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-@EnableAutoConfiguration
 public class App {
 
 	@RequestMapping("/")
